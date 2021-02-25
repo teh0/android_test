49dd6df1a7518d626dafaad48c6d747614063c8d
Théo <tochampion38@gmail.com>
2021-02-26 00:35:28 +0100

    Convert TestInstrument to kotlin

---
+++ app/src/androidTest/java/com/openclassrooms/magicgithub/UserListInstrumentedTest.kt
@@ -0,0 +1,51 @@
+package com.openclassrooms.magicgithub
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.openclassrooms.magicgithub.ui.user_list.ListUserActivity
+import com.openclassrooms.magicgithub.utils.RecyclerViewUtils.ItemCount
+import com.openclassrooms.magicgithub.utils.RecyclerViewUtils.clickChildView
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ * Testing ListUserActivity screen.
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class UserListInstrumentedTest {
+    @get:Rule // https://stackoverflow.com/questions/55496047/delegate-runner-androidx-test-internal-runner-junit4-androidjunit4classrunner
+    var mActivityRule = IntentsTestRule(ListUserActivity::class.java)
+    private var currentUsersSize = -1
+    @Before
+    fun setup() {
+        currentUsersSize = mActivityRule.activity.repository.users.size
+    }
+
+    @Test
+    fun checkIfRecyclerViewIsNotEmpty() {
+        Espresso.onView(ViewMatchers.withId(R.id.activity_list_user_rv)).check(ItemCount(currentUsersSize))
+    }
+
+    @Test
+    fun checkIfAddingRandomUserIsWorking() {
+        Espresso.onView(ViewMatchers.withId(R.id.activity_list_user_fab)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.activity_list_user_rv)).check(ItemCount(currentUsersSize + 1))
+    }
+
+    @Test
+    fun checkIfRemovingUserIsWorking() {
+        Espresso.onView(ViewMatchers.withId(R.id.activity_list_user_rv))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, clickChildView(R.id.item_list_user_delete_button)))
+        Espresso.onView(ViewMatchers.withId(R.id.activity_list_user_rv)).check(ItemCount(currentUsersSize - 1))
+    }
+}
